d7dc666b40c7f246bbcc1df223e750cafb9e790d
EliM1706 <elizabeth.minaba@campusucc.edu.co>
2025-09-01 04:48:13 +0000

    clase libro y main

--- Clase 3/Libro.java
+++ Clase 3/Libro.java
@@ -1,4 +1,24 @@
 public class Libro {
-    
-    
+    private String titulo;
+    private String autor;
+    private int precio;
+
+    public Libro(String titulo, String autor, int precio) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+    }  
+        public String toString() {
+        return "Libro { titulo: " + titulo +
+                " autor: " + autor +
+                " precio: " + precio +"}";
+    }
+     
+    public String iterarLibro(Libro[] libro) {
+        String cad = "";
+        for (int i = 0; i < libro.length; i++) {
+            cad += libro[i] + "\n";
+        }
+        return cad;
+    }
 }
---
+++ Clase 3/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Libro[] LIB = new Libro[5];
+
+        LIB [0] = new Libro("Don Quijote de la Mancha", "Miguel de Cervantes", 80000);
+        LIB [1] = new Libro("Orgullo y prejuicio", "Jane Austen", 52000);
+        LIB [2] = new Libro("Cronica de una muerte anunciada", "Gabriel Garcia Marquez", 45000);
+        LIB [3] = new Libro("Divergente", "Veronica Roth", 55000);
+        LIB [4] = new Libro("A traves de mi ventana", "Ariana Godoy", 55000);
+
+
+    }
+}
+
+
+  
+
