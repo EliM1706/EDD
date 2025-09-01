18efc27c1587025939e1566130401ea0f1db2648
EliM1706 <elizabeth.minaba@campusucc.edu.co>
2025-09-01 14:26:00 +0000

    modificaciones

--- Clase 2/EjecutarEstudiante.java
+++ Clase 2/EjecutarEstudiante.java
@@ -1,46 +1,118 @@
 import java.util.Arrays;
 
 public class EjecutarEstudiante {
-    
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //Forma 1
-        //Creación de los objetos
-        Estudiante objEst1 = new Estudiante(909876, "Santiago", "Caracas", "Ingeniería");
-        Estudiante objEst2 = new Estudiante(456098, "Elizabeth", "Mina", "Ingeniería");
-        Estudiante objEst3 = new Estudiante(235621, "Jhon", "De Jesús", "Ingeniería");
+        // creacion de los objetos
+        Estudiante objEst1 = new Estudiante(909876, "Santiago", "Caracas", "ingenieiria", "matematicas");
+        Estudiante objEst2 = new Estudiante(909877, "Johana", "Bahoz", "ingenieiria", "fisica");
+        Estudiante objEst3 = new Estudiante(909878, "Elizabeth", "Mina", "ingenieiria", "quimica");
 
         System.out.println(objEst1);
         System.out.println(objEst2);
         System.out.println(objEst3);
 
-        System.out.println("--------------------------");
-        //Crear arreglo de objetos
-        // int[] a = new int[5];
-        Estudiante[] est = new Estudiante[5];
-        
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------");
+
+        // crear arreglo de objetos
+        // int[] a = new int[5]
+        Estudiante est[] = new Estudiante[5]; // arreglo de objetos
+
         est[0] = objEst1;
         est[1] = objEst2;
         est[2] = objEst3;
 
-        //Forma 2
+        // forma2
 
-        est[3] = new Estudiante(506070, "Valeria", "Beltran", "Psicología");
-        est[4] = new Estudiante(304050, "Antonio", "Burbano", "Derecho");
+        est[3] = new Estudiante(789654, "Valeria", "Cortez", "Licenciatura", "Ingles");
+        est[4] = new Estudiante(789655, "Brandon", "Burbano", "Psicologia", "Manejo de Emociones");
 
         System.out.println(Arrays.toString(est));
 
-        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXx");
+        System.out.println("--------------------------------------------");
 
-        for(Estudiante e: est){
-            System.out.println(e);
+        for (Estudiante estudiante : est) {
+            System.out.println(estudiante);
         }
 
-        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXx");
+        System.out.println("--------------------------------------------");
 
-        for(int i = 0; i < est.length; i++){
+        for (int i = 0; i < est.length; i++) {
             System.out.println(est[i]);
+
         }
 
+        System.out.println(" --------------------------------------------");
+
+        contarEstudiantesPorFacultad(est);
+    }
+
+    public static void contarEstudiantesPorFacultad(Estudiante[] estudiantes) {
+        String[] Facultades = new String[estudiantes.length];
+        int[] conteo = new int[estudiantes.length];
+        int cantidadFacultades = 0;
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            String Facultad = estudiantes[i].getFacultad();
+            int indice = -1;
+
+            // Ver si la facultad ya está registrada
+            for (int j = 0; j < cantidadFacultades; j++) {
+                if (Facultades[j].equalsIgnoreCase(Facultad)) {
+                    indice = j;
+                    break;
+                }
+            }
+
+            // Si no está, agregarla
+            if (indice == -1) {
+                Facultades[cantidadFacultades] = Facultad;
+                conteo[cantidadFacultades] = 1;
+                cantidadFacultades++;
+            } else {
+                conteo[indice]++;
+            }
+        }
+
+        // Mostrar resultados
+        System.out.println("Cantidad de estudiantes por facultad:");
+        for (int i = 0; i < cantidadFacultades; i++) {
+            System.out.println(Facultades[i] + ": " + conteo[i]);
+        }
+
+        contarEstudiantesPorCurso(estudiantes);
+        
+    }
+
+        public static void contarEstudiantesPorCurso(Estudiante[] estudiantes){
+            String[] Cursos = new String[estudiantes.length];
+            int[] conteoCursos = new int[estudiantes.length];
+            int cantidadCursos = 0;
+
+            for(int i = 0; i < estudiantes.length; i++){
+                String Curso = estudiantes[i].getCurso();
+                int indiceCurso = -1;
+
+            for(int j = 0; j < cantidadCursos; j++){
+                if(Cursos[j].equalsIgnoreCase(Curso)){
+                    indiceCurso = j;
+                    break;
+                }
+            }
+
+            if(indiceCurso == -1){
+                Cursos[cantidadCursos] = Curso;
+                conteoCursos[cantidadCursos] = 1;
+                cantidadCursos++;
+            } else {
+                conteoCursos[indiceCurso]++;
+            }
+        }
+
+        System.out.println("Cantidad de estudiantes por curso:");
+        for(int i = 0; i < cantidadCursos; i++){
+            System.out.println(Cursos[i] + ": " + conteoCursos[i]);
+        }
     }
 }
--- Clase 2/Estudiante.java
+++ Clase 2/Estudiante.java
@@ -1,40 +1,51 @@
 public class Estudiante {
 
-    //Atributos de la clase
+     // atrubutos clase
     private int id;
     private String nombre;
     private String apellido;
     private String facultad;
+    private String curso;
 
-
-    //Constructor de la clase
-    public Estudiante(int id, String nombre, String apellido, String facultad){
+    // constructor clase = MISMO NOMBRE DE LA CLASE
+    public Estudiante(int id, String nombre, String apellido, String facultad, String curso) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.facultad = facultad;
+        this.curso = curso;
     }
 
-    public int getId(){
+    public String getCurso() {
+        return curso;
+    }
+
+    public int getId() {
         return id;
     }
 
-    public void matricularCursos(int id, String[] cursos){
-        String cad = "";
-        for(int i = 0; i < cursos.length; i++){
-            cad += cursos[i] + " ";
-        }
-        System.out.println("id:" + id + "Cursos Matriculados: " + cad );
+    public String getFacultad() {
+        return facultad;
     }
 
-    public String toString(){
-        return "Estudiante { id: " + id + " Nombre: " + nombre + " Apellido: " + apellido + " Facultad: " + facultad + " }";
-    } 
+    public void matricularCursos(int id, String[] cursos) {
+        String cad = "";
+        for (int i = 0; i < cursos.length; i++) {
+            cad += cursos[i] + " ";
+        }
+        System.out.println("id: " + id + "Cursos Matriculados: " + cad);
+    }
 
-    //implementar el método +matricularCursos(id:int, cursos[]:String):void
+    public String toString() {
+        return "Estudiante { id:" + id +
+                " Nombre: " + nombre +
+                "Apellido: " + apellido +
+                " Facultad: " + facultad +
+                " Curso: " + curso + " }"; // maquetar(estetica)
+    }
 
-    //implementar un método que permita saber cuántos estudiantes hay en cada facultad
-
-    //implementar un método que permita saber cuantos estudiantes hay en cada curso.
-    
+    // implementar el metodo +matricularCursos(id:int, cursos[]:String):void
+    // implementar un metodo que permita saber cuantos estudiantes hay en cada
+    // facultad
+    // implementar un metodo que permita saber cuantos estudiantes hay en cad curso
 }
